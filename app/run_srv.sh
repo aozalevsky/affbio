03fb1a53255962c09f57a0077f8100e95d9372be
Arthur Zalevsky <aozalevsky@gmail.com>
2014-04-12 19:52:57 +0400

    script for run on srv

---
+++ app/run_srv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Load structures
+mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/load_structures.py *.pdb
+#Calculate matrix
+mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/calculate_rmsd_matrix.py
+#Prepare matrix
+mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/prepare_cluster_matrix.py
+#Calculate and set preference
+/home/domain/silwer/work/aff_cluster/app/calculate_preference.py
+#Cluster matrix
+mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/affinity_propagation_mpi.py
