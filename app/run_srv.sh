e13b4b3b2a7bf90b9495611be75928fa33150875
Arthur Zalevsky <aozalevsky@gmail.com>
2014-04-19 15:50:41 +0400

    fix run scripts

--- app/run_srv.sh
+++ app/run_srv.sh
@@ -7,6 +7,6 @@ mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/calculate_rmsd_matrix.py
 #Prepare matrix
 mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/prepare_cluster_matrix.py
 #Calculate and set preference
-/home/domain/silwer/work/aff_cluster/app/calculate_preference.py
+/home/domain/silwer/work/aff_cluster/app/calculate_median.py
 #Cluster matrix
 mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/affinity_propagation_mpi.py
