239a20b9ee4c5baf4161543802482fff48c5eabf
Arthur Zalevsky <aozalevsky@gmail.com>
2015-01-21 23:10:58 +0300

    fixed paths

--- app/run.sh
+++ app/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+
+
 #Load structures
-mpirun -n ${1} /home/silwer/work/aff_cluster/app/load_structures.py *.pdb
+mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/load_structures.py *.pdb
 #Calculate matrix
-mpirun -n ${1} /home/silwer/work/aff_cluster/app/calculate_rmsd_matrix.py
+mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/calculate_rmsd_matrix.py
 #Prepare matrix
-mpirun -n ${1} /home/silwer/work/aff_cluster/app/prepare_cluster_matrix.py
+mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/prepare_cluster_matrix.py
 #Calculate and set preference
-/home/silwer/work/aff_cluster/app/calculate_median.py
+/home/domain/silwer/work/aff_cluster/app/calculate_median.py
 #Cluster matrix
-mpirun -n ${1} /home/silwer/work/aff_cluster/app/affinity_propagation_mpi.py
+mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/affinity_propagation_mpi.py
