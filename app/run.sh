2908bb457f60faad8190bea5fbb15f32f922b3e4
Arthur Zalevsky <aozalevsky@gmail.com>
2015-12-12 11:03:56 +0300

    run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+#Exit on first error
+set -e
+
+#Get path to app dir
+AFFDIR=$(dirname ${0})
 
 
 #Load structures
-mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/load_structures.py *.pdb
+mpirun -n ${1} ${AFFDIR}/load_structures.py *.pdb
 #Calculate matrix
-mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/calculate_rmsd_matrix.py
+mpirun -n ${1} ${AFFDIR}/calculate_rmsd_matrix.py
 #Prepare matrix
-mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/prepare_cluster_matrix.py
+mpirun -n ${1} ${AFFDIR}/prepare_cluster_matrix.py
 #Calculate and set preference
-/home/domain/silwer/work/aff_cluster/app/calculate_median.py
+${AFFDIR}/calculate_median.py
 #Cluster matrix
-mpirun -n ${1} /home/domain/silwer/work/aff_cluster/app/affinity_propagation_mpi.py
+mpirun -n ${1} ${AFFDIR}/affinity_propagation_mpi.py
 
 #print centers
-/home/domain/silwer/work/aff_cluster/app/get_stat.py
+${AFFDIR}/get_stat.py
